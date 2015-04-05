021a8e37c7d4806a8819f6225a35903e03a01087
Lukas Hinsch <lukas@hinsch.eu>
2015-04-05 17:55:54 +0200

    Initial Commit

---
+++ src/main/java/eu/hinsch/spring/propertiesdecryptor/DecryptingPropertySourcesPlaceholderConfigurer.java
@@ -0,0 +1,84 @@
+package eu.hinsch.spring.propertiesdecryptor;
+
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Created by lh on 02/04/15.
+ */
+@Component
+@ConditionalOnProperty(DecryptingPropertySourcesPlaceholderConfigurer.PASSWORD_PROPERTY)
+public class DecryptingPropertySourcesPlaceholderConfigurer extends PropertySourcesPlaceholderConfigurer {
+    static final String PASSWORD_PROPERTY = "propertyDecryption.password";
+
+    private String prefix;
+    private BasicTextEncryptor encryptor = new BasicTextEncryptor();
+
+    @Override
+    public void setEnvironment(final Environment environment) {
+        encryptor.setPassword(environment.getProperty(PASSWORD_PROPERTY));
+        prefix = environment.getProperty("propertyDecryption.prefix", "{cypher}");
+
+        final ConfigurableEnvironment configurableEnvironment = (ConfigurableEnvironment) environment;
+        final MutablePropertySources propertySources = configurableEnvironment.getPropertySources();
+
+        List<String> encryptedKeys = getKeysOfEncryptedPropertyValues(environment, propertySources);
+        addDecryptedValues(environment, propertySources, encryptedKeys);
+
+        super.setEnvironment(environment);
+    }
+
+    private List<String> getKeysOfEncryptedPropertyValues(Environment environment, MutablePropertySources propertySources) {
+        Stream<PropertySource<?>> stream = getPropertySourceStream(propertySources);
+        return stream.filter(source -> source instanceof MapPropertySource)
+                .map(source -> (MapPropertySource) source)
+                .map(PropertySource::getSource)
+                .flatMap(map -> map.entrySet().stream())
+                .map(entry -> entry.getKey())
+                .filter(key -> isEncrypted(environment.getProperty(key)))
+                .collect(toList());
+    }
+
+    private Stream<PropertySource<?>> getPropertySourceStream(final MutablePropertySources propertySources) {
+        final Iterator<PropertySource<?>> iterator = propertySources.iterator();
+        Iterable<PropertySource<?>> iterable = () -> iterator;
+        return StreamSupport.stream(iterable.spliterator(), false);
+    }
+
+    private void addDecryptedValues(Environment environment, MutablePropertySources propertySources, List<String> encryptedKeys) {
+
+        // TODO collect to properties?
+        final Properties decryptedValues = new Properties();
+        encryptedKeys.stream().forEach(
+                key -> {
+                    String value = environment.getProperty(key);
+                    decryptedValues.put(key, decryptPropertyValue(value));
+                }
+        );
+        propertySources.addFirst(new PropertiesPropertySource("decryptedValues", decryptedValues));
+    }
+
+    private String decryptPropertyValue(String encryptedPropertyValue) {
+        String cypher = getCypher(encryptedPropertyValue);
+        return encryptor.decrypt(cypher);
+    }
+
+    private boolean isEncrypted(Object propertyValue) {
+        return propertyValue != null && propertyValue instanceof String && ((String)propertyValue).startsWith(prefix);
+    }
+
+    private String getCypher(String encryptedPropertyValue) {
+        return encryptedPropertyValue.substring(prefix.length());
+    }
+}
---
+++ src/test/java/eu/hinsch/spring/propertiesdecryptor/DecryptingPropertySourcesPlaceholderConfigurerTest.java
@@ -0,0 +1,121 @@
+package eu.hinsch.spring.propertiesdecryptor;
+
+import org.jasypt.exceptions.EncryptionOperationNotPossibleException;
+import org.junit.After;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestContextManager;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
+import static org.junit.Assert.assertThat;
+import static org.junit.internal.matchers.ThrowableCauseMatcher.hasCause;
+
+/**
+ * Created by lh on 04/04/15.
+ */
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@SpringApplicationConfiguration(classes = DecryptingPropertySourcesPlaceholderConfigurerTest.TestConfig.class)
+public class DecryptingPropertySourcesPlaceholderConfigurerTest {
+
+    private static final String CODE = "Cn0fGsAEINM1+FsMf0evHob485wqmPwT";
+    private static final String ENCRYPTED_VALUE = "{cypher}" + CODE;
+    private static final String SECRET = "MY-SECRET";
+    private List<String> systemProperties = new ArrayList<>();
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Autowired
+    private Environment environment;
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    private TestContextManager testContextManager;
+
+    @SpringBootApplication
+    static class TestConfig {
+    }
+
+    @Test
+    public void shouldDecodeSystemProperty() throws Exception {
+        // given
+        setSystemProperty("secretProperty", ENCRYPTED_VALUE);
+
+        // when
+        startupContext();
+
+        // then
+        assertThat(environment.getProperty("secretProperty"), is(SECRET));
+    }
+
+    @Test
+    public void shouldDecryptCorrectPropertyOnConflict() throws Exception {
+        // given
+        setSystemProperty("conflictingProperty", ENCRYPTED_VALUE);
+
+        // when
+        startupContext();
+
+        //
+        assertThat(environment.getProperty("conflictingProperty"), is(SECRET));
+    }
+
+    @Test
+    public void shouldThrowExceptionIfPasswordIsIncorrect() throws Exception {
+        // given
+        setSystemProperty("propertyEncryption.password", "NOT-THE-CORRECT-PASSWORD");
+        setSystemProperty("somethingToDecrypt", ENCRYPTED_VALUE);
+        exception.expect(IllegalStateException.class);
+        // meaningful exception is wrapped twice inside spring errors
+        exception.expectCause(hasCause(isA(EncryptionOperationNotPossibleException.class)));
+
+        // when
+        startupContext();
+
+        // then -> exception
+    }
+
+    @Test
+    @Ignore("TODO not yet working")
+    public void shouldUsePrefixOverride() throws Exception {
+        // given
+        setSystemProperty("propertyDecryption.prefix", "{cypher}");
+        setSystemProperty("secretProperty", ENCRYPTED_VALUE);
+
+        // when
+        startupContext();
+
+        // then
+        assertThat(environment.getProperty("secretProperty"), is(SECRET));
+    }
+
+    private void setSystemProperty(String property, String value) {
+        System.setProperty(property, value);
+        systemProperties.add(property);
+    }
+
+    // manual app startup because we want to set properties before
+    private void startupContext() throws Exception {
+        testContextManager = new TestContextManager(this.getClass());
+        testContextManager.prepareTestInstance(this);
+        testContextManager.registerTestExecutionListeners(new DirtiesContextTestExecutionListener());
+    }
+
+    @After
+    public void cleanUp() {
+        systemProperties.stream().forEach(System::clearProperty);
+    }
+}
